ac99e17501ab21c78fe98e6878686364526d9d8c
JustineT. <justine.tesara0907@gmail.com>
2025-11-17 20:42:49 +0800

    Initial Commit

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
+import ynab from "ynab";
+
+const accessToken = "g3vwvFNV-28L3i9Tt5O4EulF1vf4d--dgtCZT5r1W2o";
+const client = new ynab.API(accessToken);
+
+const budgets = await client.budgets.getBudgets();
+console.log(budgets.data);
 
 const app = express();
 const PORT = 3000;
