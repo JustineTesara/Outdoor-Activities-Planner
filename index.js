d2c993e389e38f3aa33ef09c577417a34e624394
JustineT. <justine.tesara0907@gmail.com>
2025-11-17 20:15:10 +0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
