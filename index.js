0d86c1c00ea367e9dda2bf75eb9a40a5509c057f
JustineT. <justine.tesara0907@gmail.com>
2025-11-18 20:46:04 +0800

    Change API

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
-import ynab from "ynab";
 
 const accessToken = "g3vwvFNV-28L3i9Tt5O4EulF1vf4d--dgtCZT5r1W2o";
 const client = new ynab.API(accessToken);
-
-const budgets = await client.budgets.getBudgets();
-console.log(budgets.data);
 
 const app = express();
 const PORT = 3000;
@@ -17,7 +13,7 @@ app.use(bodyParser.json());
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index");
 });
 
 app.listen(PORT, () => {
